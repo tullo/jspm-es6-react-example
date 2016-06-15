f6375a4917b6575a0c04c9b55902590e8eb05578
Andreas Amstutz <andreasamstutz@gmail.com>
2016-06-15 12:30:22 +0200

    Condtitional delete of dist dir

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rm -r dist
-mkdir dist
+if [ -d ./dist ]; then
+rm -rv dist
+fi
+mkdir -v dist
 jspm bundle-sfx app/main.js dist/app.js --minify --skip-source-maps
 #./node_modules/.bin/uglifyjs dist/app.js -o dist/app.min.js
 ./node_modules/.bin/html-dist --config html-dist.config.js --input index.html
